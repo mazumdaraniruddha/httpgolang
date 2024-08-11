08bf32bb536d04c44a2e3124f4092d151675960a
PhoBi <phobosforever@gmail.com>
2024-08-12 01:13:58 +0530

    Added basic server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("test")
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Sample response\n"))
 }
